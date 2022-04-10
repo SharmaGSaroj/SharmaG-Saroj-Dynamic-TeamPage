9aeca95426308c9b5884a773acc7bf456e536fa9
SharmaGSaroj <91160959+SharmaGSaroj@users.noreply.github.com>
2022-04-09 21:15:06 -0400

    JavaScript Update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+function saroj() {
+    var g = document.getElementById("sharma");
+    if (g.style.display === "none") {
+      g.style.display = "block";
+    } else {
+      g.style.display = "none";
+    }
+  }
+
+
+  function KateBishop() {
+    var kate = document.getElementById("kateB");
+    if (kate.style.display === "none") {
+      kate.style.display = "block";
+    } else {
+      kate.style.display = "none";
+    }
+
+
+  }
